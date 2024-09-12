14f14e41c42fc140f9144a6a63da446d35dda352
ElBoyTM <nszalenski@gmail.com>
2024-09-12 13:19:13 -0400

    added formSubmission to form.js

--- Develop/assets/js/form.js
+++ Develop/assets/js/form.js
@@ -1,5 +1,9 @@
 // TODO: Create a variable that selects the form element
-
+const formSubmission = {
+    username: document.getElementById('username'),
+    title: document.getElementById('title'),
+    content: document.getElementById('content'),
+};
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
