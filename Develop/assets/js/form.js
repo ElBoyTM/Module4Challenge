ead9578d9a02b4b49f4215591f2c748cde78c4e7
ElBoyTM <nszalenski@gmail.com>
2024-09-12 18:34:40 -0400

    lots of stuff

--- Develop/assets/js/form.js
+++ Develop/assets/js/form.js
@@ -3,14 +3,15 @@
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
-readLocalStorage.addEventListener('click', function (event) {
+document.querySelector('#submission').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formSubmission = {
-        username: document.getElementById('username'),
-        title: document.getElementById('title'),
-        content: document.getElementById('content').trim(),
+        username: document.getElementById('username').value.trim(),
+        title: document.getElementById('title').value.trim(),
+        content: document.getElementById('content').value.trim(),
     };
 
-    localStorage.setItem('formSubmission', JSON.stringify(formSubmission));
+    // localStorage.setItem('formSubmission', JSON.stringify(formSubmission));
+    // add to logic
 });
