f22d9722bcfed0f4ab9ad2ab357c5395e9c34a66
ElBoyTM <nszalenski@gmail.com>
2024-09-13 17:24:14 -0400

    moved let formArray outside listener; we now have arrays

--- Develop/assets/js/form.js
+++ Develop/assets/js/form.js
@@ -3,10 +3,10 @@
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+let formArray = [];
+
 document.querySelector('#submission').addEventListener('submit', function (event) {
     event.preventDefault();
-
-    let formArray = [];
 
     const formSubmission = {
         username: document.getElementById('username').value.trim(),
